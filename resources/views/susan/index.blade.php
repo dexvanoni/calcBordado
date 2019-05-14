22a4ab25288f585d6eb051a1f138e97180ba854e
dexvanoni <dex.vanoni@gmail.com>
2019-05-14 15:33:32 -0400

    Alteração 3 - Home Page

--- resources/views/susan/index.blade.php
+++ resources/views/susan/index.blade.php
@@ -20,8 +20,8 @@
     <!-- HTmL5 shim and Respond.js for IE8 support of HTmL5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <script src="susan/https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="susan/https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
 </head>
 
@@ -55,7 +55,7 @@
                 <div class="col-md-4 col-sm-6 col-xs-12">
                     <a href="theme/index.html" target="_blank">
                         <div class="item-imag">
-                            <img src="susan/image/product/index-1.png" class="img-responsive radius" alt="">
+                            <img src="susan/susan/image/product/index-1.png" class="img-responsive radius" alt="">
                             <p class="product-title">OnePage - 01</p>
                         </div>
                     </a>
@@ -63,7 +63,7 @@
                 <div class="col-md-4 col-sm-6 col-xs-12">
                     <a href="theme/index-2.html" target="_blank">
                         <div class="item-imag">
-                            <img src="image/product/index-2.png" class="img-responsive radius" alt="">
+                            <img src="susan/susan/image/product/index-2.png" class="img-responsive radius" alt="">
                             <p class="product-title">OnePage - 02</p>
                         </div>
                     </a>
@@ -80,7 +80,7 @@
                 <div class="col-md-3 col-sm-6 col-xs-12">
                     <a href="theme/blog-list.html" target="_blank">
                         <div class="item-imag">
-                            <img src="image/product/blog-1.png" class="img-responsive radius" alt="">
+                            <img src="susan/image/product/blog-1.png" class="img-responsive radius" alt="">
                             <p class="product-title">Blog List - 01</p>
                         </div>
                     </a>
@@ -88,7 +88,7 @@
                 <div class="col-md-3 col-sm-6 col-xs-12">
                     <a href="theme/blog-list-2.html" target="_blank">
                         <div class="item-imag">
-                            <img src="image/product/blog-2.png" class="img-responsive radius" alt="">
+                            <img src="susan/image/product/blog-2.png" class="img-responsive radius" alt="">
                             <p class="product-title">Blog List - 02</p>
                         </div>
                     </a>
@@ -96,7 +96,7 @@
                 <div class="col-md-3 col-sm-6 col-xs-12">
                     <a href="theme/blog-details.html" target="_blank">
                         <div class="item-imag">
-                            <img src="image/product/blog-3.png" class="img-responsive radius" alt="">
+                            <img src="susan/image/product/blog-3.png" class="img-responsive radius" alt="">
                             <p class="product-title">Blog Details - 01</p>
                         </div>
                     </a>
@@ -104,7 +104,7 @@
                 <div class="col-md-3 col-sm-6 col-xs-12">
                     <a href="theme/blog-details-2.html" target="_blank">
                         <div class="item-imag">
-                            <img src="image/product/blog-4.png" class="img-responsive radius" alt="">
+                            <img src="susan/image/product/blog-4.png" class="img-responsive radius" alt="">
                             <p class="product-title">Blog Details - 02</p>
                         </div>
                     </a>
@@ -120,18 +120,18 @@
         </div>
     </footer>
     <!--End Footer area-->
-    <script src="susan/js/jquery-2.2.4.min.js"></script>
-    <script src="susan/js/bootstrap.min.js"></script>
-    <script src="susan/js/nav.js"></script>
+    <script src="susan/susan/js/jquery-2.2.4.min.js"></script>
+    <script src="susan/susan/js/bootstrap.min.js"></script>
+    <script src="susan/susan/js/nav.js"></script>
     <!--waypoint js-->
-    <script src="susan/vendores/waypoint/waypoints.min.js"></script>
+    <script src="susan/susan/vendores/waypoint/waypoints.min.js"></script>
     <!--counter js-->
-    <script src="susan/vendores/couterup/jquery.counterup.min.js"></script>
-    <script src="susan/vendores/typedjs/typed.min.js"></script>
-    <script src="susan/vendores/ripples/jquery.ripples-min.js"></script>
-    <script type="text/javascript" src="susan/js/scrollIt.min.js"></script>
-    <script type="text/javascript" src="susan/js/wow.js"></script>
-    <script src="susan/js/custom.js"></script>
+    <script src="susan/susan/vendores/couterup/jquery.counterup.min.js"></script>
+    <script src="susan/susan/vendores/typedjs/typed.min.js"></script>
+    <script src="susan/susan/vendores/ripples/jquery.ripples-min.js"></script>
+    <script type="text/javascript" src="susan/susan/js/scrollIt.min.js"></script>
+    <script type="text/javascript" src="susan/susan/js/wow.js"></script>
+    <script src="susan/susan/js/custom.js"></script>
     <script>
     $(function() {
         $.scrollIt();
