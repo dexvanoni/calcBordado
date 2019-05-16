2b0e8d8dd4a6edfcf19b016f348b722ca81316d8
dexvanoni <dex.vanoni@gmail.com>
2019-05-16 09:56:29 -0400

    Alterei Home

--- resources/views/susan/index-2.blade.php
+++ resources/views/susan/index-2.blade.php
@@ -55,7 +55,7 @@
                     <!-- Collect the nav links, forms, and other content for toggling -->
                     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                         <ul class="nav navbar-nav navbar-right">
-                            <li><a href="#home">Home</a></li>
+                            <li><a href="#home">Home - teste</a></li>
                             <li><a href="#feature">Calculadora</a></li>
                             <li><a href="#screenshot">Imagens</a></li>
                             <li><a href="#feedback">Feedback</a></li>
